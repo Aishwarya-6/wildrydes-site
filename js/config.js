14507d599887ef3c3fa759a6d6e13e14d0629e61
Aishwarya-6 <79437335+Aishwarya-6@users.noreply.github.com>
2025-04-22 03:09:44 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_4m1u6Mf7J', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '48oq7v8693bdla924bv2j9np2i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
